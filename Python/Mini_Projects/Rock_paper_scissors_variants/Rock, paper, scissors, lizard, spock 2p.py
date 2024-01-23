ceef84f755dec01bc3c167e791801d8558201d29
ThWetCookie <lennert.dewijngaert@gmail.com>
2024-01-23 18:39:59 +0100

    Added the rules (again)

--- Python/Mini_Projects/Rock_paper_scissors_variants/Rock, paper, scissors, lizard, spock 2p.py
+++ Python/Mini_Projects/Rock_paper_scissors_variants/Rock, paper, scissors, lizard, spock 2p.py
@@ -1,5 +1,17 @@
 import os
 
+""" Scissors cuts Paper
+Paper covers Rock
+Rock crushes Lizard
+Lizard poisons Spock
+Spock smashes Scissors
+Scissors decapitates Lizard
+Lizard eats Paper
+Paper disproves Spock
+Spock vaporizes Rock
+(and as it always has) Rock crushes Scissors """
+
+
 def get_input_and_clear(prompt=""):
     print(prompt, end='', flush=True)
     user_input = input()
